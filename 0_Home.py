f3b48acf48cb3a1f80705289d1a38f9e0616394c
Sang-UIT <20520736@gm.uit.edu.vn>
2022-10-22 11:58:20 +0700

    add cm

---
+++ 0_Home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Blog's Sang",
+    page_icon="👋",
+)
+
+st.write("# Chào mừng đến với blog của Sang! 👋")
+
+st.sidebar.success("Chọn chỉ mục muốn xem.")
+
+st.markdown(
+    """
+    Blog này là gì?
+    **Đây là blog để mình đăng tải thơ, văn và các dự án cá nhân của bản thân**
+    ### Liên hệ với mình?
+    Email: tnsangtrng@gmail.com
+"""
+)
---
+++ pages/1_Thơ.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Thơ",
+)
+st.sidebar.success("Chọn thơ muốn xem.")
