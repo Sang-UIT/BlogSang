f423ee371ab919276b0b0dda0b06af4803d5b022
Sang-UIT <20520736@gm.uit.edu.vn>
2022-10-22 16:41:54 +0700

    add page tho

--- pages/1_Thơ.py
+++ pages/1_Thơ.py
@@ -2,5 +2,31 @@ import streamlit as st
 st.set_page_config(
     page_title="Thơ",
 )
-st.sidebar.success("Chọn thơ muốn xem.")
+st.sidebar.write("# Những bài thơ của Sang sẽ được tổng hợp ở đây")
+st.sidebar.caption("2022")
+
+st.sidebar.button("Chẳng biết phải đặt tên như thế nào")
+
+st.sidebar.caption("2021")
+
+st.sidebar.button("Con đã về đây, với mẹ thân yêu")
+st.sidebar.button("Tấm gương")
+st.sidebar.button("Sài gòn vắng em...")
+
+st.sidebar.caption("2020")
+
+st.sidebar.button("Mẹ...")
+st.sidebar.button("[Dịch thơ] Do not stand at my grave and weep")
+st.sidebar.button("Thơ về tác phẩm Vợ Nhặt")
+st.sidebar.button("Thơ về Mị")
+
+st.sidebar.caption("2019")
+
+st.sidebar.button("Tôi lại bâng khuâng giữa mây ngàn")
+st.sidebar.button("Gửi gió cùng những yêu thương")
+
+st.sidebar.caption("2018")
+
+st.sidebar.button("Hoa lục bình")
+
 
