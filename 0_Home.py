83867e710c7088707eb0656792ab329028a7d450
Sang-UIT <20520736@gm.uit.edu.vn>
2022-10-22 16:58:19 +0700

    fix home

--- 0_Home.py
+++ 0_Home.py
@@ -13,6 +13,19 @@ st.markdown(
     """
     Blog này là gì?
     **Đây là blog để mình đăng tải thơ, văn và các dự án cá nhân của bản thân**
+    ## Nhấn vào nút phía bên trái để xem các tùy chọn hoặc nhấn vào đây 👇
+    """)
+col1,col2,col3,col4 = st.columns([1,2,2,5])
+with col1:
+    st.button("Thơ")
+with col2:
+    st.button("Truyện Ngắn")
+with col3:
+    st.button("Tiểu Thuyết")
+
+
+
+st.markdown("""
     ### Liên hệ với mình?
     Email: tnsangtrng@gmail.com
 """
